80036a123d37707062e4e69e364f647c6468597b
Ryan Lee <gxlhybh@gmail.com>
2026-02-06 00:55:54 +0400

    refactor: group case execution input fields

--- judge_client_case_executor.cpp
+++ judge_client_case_executor.cpp
@@ -15,45 +15,38 @@ using namespace std;
 extern int call_counter[call_array_size];
 
 CaseExecutionOutput execute_single_case(const CaseExecutionInput &input) {
-    CaseExecutionOutput output;
-    output.ACflg = input.ACflg;
-    output.PEflg = input.PEflg;
-    output.topmemory = input.topmemory;
-    output.pass_point = input.pass_point;
-    output.finalACflg = input.finalACflg;
-    output.pass_rate = input.pass_rate;
-    output.usedtime = input.usedtime;
-    output.max_case_time = input.max_case_time;
+    CaseExecutionOutput output = input.state;
 
-    prepare_files(input.infile_pair->first.c_str(), input.infile_pair->second, input.infile, input.ctx.p_id,
-                  input.work_dir, input.outfile, input.userfile, input.runner_id);
-    if (input.syscall_template) {
-        memcpy(call_counter, input.syscall_template, sizeof(int) * call_array_size);
+    prepare_files(input.io.infile_pair->first.c_str(), input.io.infile_pair->second, input.io.infile, input.ctx.p_id,
+                  input.io.work_dir, input.io.outfile, input.io.userfile, input.runner_id);
+    if (input.io.syscall_template) {
+        memcpy(call_counter, input.io.syscall_template, sizeof(int) * call_array_size);
     } else {
         InitManager::initSyscallLimits(input.ctx.lang, call_counter, input.ctx.flags.record_call != 0, call_array_size);
     }
     pid_t pidApp = spawn_child([&]() {
-        run_solution(input.ctx.lang, input.work_dir, input.time_limit, output.usedtime, input.memory_limit,
+        run_solution(input.ctx.lang, input.io.work_dir, input.limits.time_limit, output.usedtime, input.limits.memory_limit,
                      input.ctx.config, input.ctx.language_factory);
     });
     if (pidApp != CHILD_PROCESS) {
-        watch_solution_ex(pidApp, input.infile, output.ACflg, input.ctx.special_judge, input.userfile, input.outfile,
-                          input.solution_id, input.ctx.lang, output.topmemory, input.memory_limit, output.usedtime,
-                          input.time_limit, input.ctx.p_id, output.PEflg, input.work_dir, input.ctx.config, input.ctx.env,
-                          input.ctx.flags.record_call != 0, input.ctx.flags.debug != 0);
-        judge_solution(input.ctx, output.ACflg, output.usedtime, input.time_limit, input.ctx.special_judge, input.infile,
-                       input.outfile, input.userfile, input.usercode, output.PEflg, input.work_dir, output.topmemory,
-                       input.memory_limit, input.solution_id, input.num_of_test, *input.global_work_dir);
+        watch_solution_ex(pidApp, input.io.infile, output.ACflg, input.ctx.special_judge, input.io.userfile, input.io.outfile,
+                          input.solution_id, input.ctx.lang, output.topmemory, input.limits.memory_limit, output.usedtime,
+                          input.limits.time_limit, input.ctx.p_id, output.PEflg, input.io.work_dir, input.ctx.config,
+                          input.ctx.env, input.ctx.flags.record_call != 0, input.ctx.flags.debug != 0);
+        judge_solution(input.ctx, output.ACflg, output.usedtime, input.limits.time_limit, input.ctx.special_judge,
+                       input.io.infile, input.io.outfile, input.io.userfile, input.io.usercode, output.PEflg,
+                       input.io.work_dir, output.topmemory, input.limits.memory_limit, input.solution_id,
+                       input.limits.num_of_test, *input.io.global_work_dir);
         if (input.ctx.config.use_max_time) {
             output.max_case_time = max(output.usedtime, output.max_case_time);
             output.usedtime = ZERO_TIME;
         }
     }
 
-    if (output.usedtime > input.time_limit * 1000 || output.ACflg == TIME_LIMIT_EXCEEDED) {
+    if (output.usedtime > input.limits.time_limit * 1000 || output.ACflg == TIME_LIMIT_EXCEEDED) {
         cout << "Time Limit Exceeded" << endl;
         output.ACflg = TIME_LIMIT_EXCEEDED;
-        output.usedtime = input.time_limit * 1000;
+        output.usedtime = input.limits.time_limit * 1000;
     }
 
     if (output.ACflg == ACCEPT) {
--- judge_client_case_executor.h
+++ judge_client_case_executor.h
@@ -5,13 +5,13 @@
 
 #include "judge_client_context.h"
 
-struct CaseExecutionInput {
-    JudgeContext &ctx;
-    int runner_id = 0;
-    int solution_id = 0;
+struct CaseExecutionLimits {
     int num_of_test = 0;
     int memory_limit = 0;
     double time_limit = 0.0;
+};
+
+struct CaseExecutionIO {
     char *work_dir = nullptr;
     char *infile = nullptr;
     char *outfile = nullptr;
@@ -20,6 +20,9 @@ struct CaseExecutionInput {
     std::string *global_work_dir = nullptr;
     const int *syscall_template = nullptr;
     const std::pair<std::string, int> *infile_pair = nullptr;
+};
+
+struct CaseExecutionState {
     int ACflg = ACCEPT;
     int PEflg = ACCEPT;
     int topmemory = ZERO_MEMORY;
@@ -30,15 +33,15 @@ struct CaseExecutionInput {
     double max_case_time = ZERO_TIME;
 };
 
-struct CaseExecutionOutput {
-    int ACflg = ACCEPT;
-    int PEflg = ACCEPT;
-    int topmemory = ZERO_MEMORY;
-    int pass_point = ZERO_PASSPOINT;
-    int finalACflg = ACCEPT;
-    double pass_rate = ZERO_PASSRATE;
-    double usedtime = ZERO_TIME;
-    double max_case_time = ZERO_TIME;
+struct CaseExecutionInput {
+    JudgeContext &ctx;
+    int runner_id = 0;
+    int solution_id = 0;
+    CaseExecutionLimits limits;
+    CaseExecutionIO io;
+    CaseExecutionState state;
 };
+
+using CaseExecutionOutput = CaseExecutionState;
 
 CaseExecutionOutput execute_single_case(const CaseExecutionInput &input);
